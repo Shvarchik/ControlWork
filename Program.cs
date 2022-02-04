8e604a14f9d9954cd76d698ace664a839ecbfa3a
Inna <shvartz_i@mail.ru>
2022-02-05 00:14:44 +0300

    отдельным методом заранее считаем размер массива результата

--- Program.cs
+++ Program.cs
@@ -5,32 +5,44 @@
 // пример ["1234","-2","Kazan", ":)"] -> ["-2",":)"]
 
 
-void PrintArray (string[] array, int length)
+void PrintArray (string[] array)
 {
+    int length = array.Length;
     Console.Write("[");
     for (int i = 0; i < length - 1; i++)
         Console.Write($"'{array[i]}', ");
     Console.WriteLine($"'{array[length - 1]}']");
 }
-int CreateNewArray (string [] array, string [] newArray)
+
+void CreateNewArray (string [] array, string [] newArray)
 {
-    int newCount = 0;               // счетчик заполненных элементов нового массива
-    for (int i=0; i < array.Length && newCount < newArray.Length ;i++)
+    int countNewArray = 0;
+    for (int i=0; i < array.Length; i++)
     {
         if (array[i].Length <=3)
         {
-            newArray[newCount] = array[i];
-            newCount++;
+            newArray[countNewArray] = array[i];
+            countNewArray++;
         }
+    }
+} 
+int CountNewArray (string [] array)
+{
+    int newCount = 0;               // счетчик элементов, удовлетворяющих условию
+    for (int i=0; i < array.Length;i++)
+    {
+        if (array[i].Length <=3)
+            newCount++;
     }
     return newCount;
 } 
+
 string[] stringArray = new string[] { "1234", "-2", "Kazan", ":)" };
 
-string [] ResultArray = new string [50];
-int countResult = CreateNewArray (stringArray,ResultArray);
+string [] ResultArray = new string [CountNewArray(stringArray)];
+CreateNewArray (stringArray,ResultArray);
 
 Console.WriteLine ("Исходный массив:");
-PrintArray (stringArray,stringArray.Length);
+PrintArray (stringArray);
 Console.WriteLine ("Массив результат:");
-PrintArray (ResultArray,countResult);
+PrintArray (ResultArray);
