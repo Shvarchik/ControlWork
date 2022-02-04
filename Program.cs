d0637d7349da73f5bb1568c884ead98dbce4846f
Inna <shvartz_i@mail.ru>
2022-02-05 02:50:08 +0300

    Program added

--- Program.cs
+++ Program.cs
@@ -4,8 +4,29 @@
 // либо задать на старте выполнения алгоритма.
 // пример ["1234","-2","Kazan", ":)"] -> ["-2",":)"]
 
-
-void PrintArray (string[] array)
+int CountNewArray(string[] array)
+{
+    int newCount = 0;               // счетчик элементов, удовлетворяющих условию
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            newCount++;
+    }
+    return newCount;
+}
+void CreateNewArray(string[] array, string[] newArray)
+{
+    int countNewArray = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[countNewArray] = array[i];
+            countNewArray++;
+        }
+    }
+}
+void PrintArray(string[] array)
 {
     int length = array.Length;
     Console.Write("[");
@@ -14,36 +35,10 @@ void PrintArray (string[] array)
     Console.WriteLine($"'{array[length - 1]}']");
 }
 
-void CreateNewArray (string [] array, string [] newArray)
-{
-    int countNewArray = 0;
-    for (int i=0; i < array.Length; i++)
-    {
-        if (array[i].Length <=3)
-        {
-            newArray[countNewArray] = array[i];
-            countNewArray++;
-        }
-    }
-} 
-int CountNewArray (string [] array)
-{
-    int newCount = 0;               // счетчик элементов, удовлетворяющих условию
-    for (int i=0; i < array.Length;i++)
-    {
-        if (array[i].Length <=3)
-            newCount++;
-    }
-    return newCount;
-} 
-
 string[] stringArray = new string[] { "1234", "-2", "Kazan", ":)" };
-
-DateTime dt = DateTime.Now;
-string [] ResultArray = new string [CountNewArray(stringArray)];
-CreateNewArray (stringArray,ResultArray);
-Console.WriteLine ("Исходный массив:");
-PrintArray (stringArray);
-Console.WriteLine ("Массив результат:");
-PrintArray (ResultArray);
-System.Console.WriteLine((DateTime.Now-dt).TotalMilliseconds);
+string[] ResultArray = new string[CountNewArray(stringArray)];
+CreateNewArray(stringArray, ResultArray);
+Console.WriteLine("Исходный массив:");
+PrintArray(stringArray);
+Console.WriteLine("Массив результат:");
+PrintArray(ResultArray);
